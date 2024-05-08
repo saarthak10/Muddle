7959c7e861d94837e1e841032eb717642684fb1d
saarthak10 <saarthaksharma87@gmail.com>
2024-05-09 01:27:15 +0530

    splash screen setup done, facing issue in kapt

--- app/src/main/java/com/learn/muddle/MainActivity.kt
+++ app/src/main/java/com/learn/muddle/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.learn.muddle.ui.theme.MuddleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
              MuddleTheme {
                 // A surface container using the 'background' color from the theme
