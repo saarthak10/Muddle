d72c035f6e2e6dfee5c4a4372a5e073d92882446
saarthak10 <saarthaksharma87@gmail.com>
2024-05-10 01:18:30 +0530

    splash working fine, moving to building onboarding screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -67,7 +67,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.dagger.hilt)
-    implementation(libs.dagger.hilt.compiler)
     implementation(libs.splash)
 
 }
---
+++ app/src/main/java/com/learn/muddle/presentation/onboarding/OnboardingScreen.kt
@@ -0,0 +1,23 @@
+package com.learn.muddle.presentation.onboarding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.learn.muddle.ui.theme.MuddleTheme
+
+@Composable
+fun OnboardingScreen(){
+    Column() {
+
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun OnboardingScreenPreview(){
+    MuddleTheme {
+        OnboardingScreen()
+    }
+}
